fb704bd0af9a1c7432795cb125b29e8ddf8b23ac
kishu279 <souravpoddar93042@gmail.com>
2026-02-21 14:19:47 +0530

    added the minimal user table

--- app/sourav/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Sourav() {
-    return <div className="bg-blue-500 p-4 align-items items-center justify-center flex h-screen flex-col">
-        <h1 className="text-white text-2xl">sourav this side</h1>
-        <p className="text-black p-5 border rounded-2xl" >let me know who am i </p>
-        <h1 className="text-white text-2xl">sourav this side</h1>
-        <p className="text-black p-5 border rounded-2xl" >let me know who am i </p>
-
-    </div>
-}
